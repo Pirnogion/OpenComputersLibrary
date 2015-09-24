f30931c9a36721c60534e0bbeed90a013f425741
Pirnogion <glebprositapow@gmail.com>
2015-09-24 21:52:52 +0500

    Update example.lua

--- ocif/example.lua
+++ ocif/example.lua
@@ -11,11 +11,11 @@ local ocif_image_raw = {
 	[IMAGE_WIDTH] = 8, --Ширина изображения
 	[IMAGE_HEIGHT] = 5, --Высота изображения
 	[IMAGE] = { 
-		0x000000, 0xFFFFFF, 200, '1',
-		0x000000, 0xFFFFFF, 200, '0', 
-		0x000000, 0xFFFFFF, 200, 'З',
-		0x000000, 0xFFFFFF, 200, '0',
-		0x000000, 0xFFFFFF, 200, '1',
+		0x000000, 0x003f49, 0, '1',
+		0x000000, 0x338592, 0, '0', 
+		0x000000, 0x338592, 0, 'З',
+		0x000000, 0x003f49, 0, '0',
+		0x000000, 0x003f49, 0, '1',
 		0x000000, 0x004980, 200, ' ',
 		0x000000, 0x004980, 200, ' ', 
 		0x000000, 0x004980, 200, ' ',
@@ -63,14 +63,14 @@ ocif.write("ocif_test.ocif", ocif_image_raw, true)
 --Чтение изображения(получили массив изображения в "неудобном" формате, т.е основном)
 local ocif_image = ocif.read("ocif_test.ocif")
 --Вывод изображения
-ocif.draw( ocif_image, 16, 13, gpu )
+ocif.draw( ocif_image, 2, 1, gpu )
 
 --Запись в файл сырого изображения в "неудобном" формате(который был ранее уже прочитан из файла)
 ocif.write("ocif_test.ocif", ocif_image)
 --Чтение изображения
 local ocif_image = ocif.read("ocif_test.ocif")
 --Вывод изображения
-ocif.draw( ocif_image, 36, 15, gpu )
+ocif.draw( ocif_image, 11, 1, gpu )
 
 --Для дебага--
 gpu.setBackground(0)
