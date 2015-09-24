8554d896e4ce722a8ddc483c05b794c66bf779fc
Pirnogion <glebprositapow@gmail.com>
2015-09-24 21:51:16 +0500

    Update color conversion algorithms
    Updated HEX_24to8 and HEX_8to24. Added support custom palettes(Used 6-8-5 indexed color + grayscale). MINIMUM LOSSES COLORS!

--- ocif/ocif.lua
+++ ocif/ocif.lua
@@ -3,27 +3,38 @@
 --------------------
 
 --CONSTANTS--
-local IMAGE_FG = 3
-local IMAGE_BG = 2
-local IMAGE_AL = 1
+local FOREGROUND = 3
+local BACKGROUND = 2
+local ALPHA_CHANNEL = 1
 
-local IMAGE_CH = 0
-local IMAGE_COMPRESS = 1
+local UTF8_CHAR = 0
+local COMPRESSED_PIXEL = 1
 
 local ELEMENT_COUNT = 2
-local ELEMENT_COUNT_HF = 4
+local ELEMENT_COUNT_FULL_ARRAY = 4
 
 local IMAGE_WIDTH  = 1
 local IMAGE_HEIGHT = 2
 local IMAGE        = 3
 
-local BYTE      = 8
+local BYTE_SIZE = 8
 local NULL_CHAR = 0
 
 local FILE_OPEN_ERROR = "Can't open file"
+local LOAD_PALETTE_ERROR = "Can't load color index array. Table = nil or Table size less 256[0-255]"
+local SIGNATURE_MATCH_ERROR = "Signatures not match"
+
+--Загрузка палитры
+local palette = nil
+function LOAD_PALETTE( color_index_array )
+	palette = color_index_array
+end
+dofile("palette.cia")
+if ( not palette or #palette ~= 255 ) then
+	error( LOAD_PALETTE_ERROR )
+end
 
 --.ocif подпись
---.ocif signature
 local ocif_signature1 = 0x896F6369
 local ocif_signature2 = 0x00661A0A --7 bytes: 89 6F 63 69 66 1A 0A
 local ocif_signature_expand = { string.char(0x89), string.char(0x6F), string.char(0x63), string.char(0x69), string.char(0x66), string.char(0x1A), string.char(0x0A) }
@@ -31,7 +42,6 @@ local ocif_signature_expand = { string.char(0x89), string.char(0x6F), string.cha
 local imageAPI = {}
 
 --Выделить бит-терминатор в первом байте utf8 символа: 1100 0010 --> 0010 0000
---Select terminate bit in the first byte utf8 char: 1100 0010 --> 0010 0000
 local function selectTerminateBit_l()
 	local prevByte = nil
 	local prevTerminateBit = nil
@@ -60,8 +70,7 @@ local function selectTerminateBit_l()
 end
 local selectTerminateBit = selectTerminateBit_l()
 
---Прочитать n байтов из файла, возвращает прочитанные байты как число, если не удалось прочитать, то возвращает 0
---Read n byte from file and returns readed bytes as number. If read failed, then returns 0
+--Прочитать n <= 4 байтов из файла, возвращает прочитанные байты как число, если не удалось прочитать, то возвращает 0
 local function readBytes(file, bytes)
   local readedByte = 0
   local readedNumber = 0
@@ -74,7 +83,6 @@ local function readBytes(file, bytes)
 end
 
 --Преобразует цвет в hex записи в rgb запись
---Convert HEX color to RGB color
 function HEXtoRGB(color)
   local rr = bit32.rshift( color, 16 )
   local gg = bit32.rshift( bit32.band(color, 0x00ff00), 8 )
@@ -88,71 +96,73 @@ function RGBtoHEX(rr, gg, bb)
 end
 
 --Альфа-смешение
---Alpha blending
-local function alpha_blend(back_color, front_color, alpha_channel)
+local function alphaBlend(background_screen_color, foreground_image_color, alpha_channel)
+	if ( alpha_channel < 0 ) then return foreground_image_color end
+
 	local INVERTED_ALPHA_CHANNEL = 255-alpha_channel
 
-	local back_color_rr, back_color_gg, back_color_bb    = HEXtoRGB(back_color)
-	local front_color_rr, front_color_gg, front_color_bb = HEXtoRGB(front_color)
+	local background_screen_color_rr, background_screen_color_gg, background_screen_color_bb    = HEXtoRGB(background_screen_color)
+	local foreground_image_color_rr, foreground_image_color_gg, foreground_image_color_bb = HEXtoRGB(foreground_image_color)
 
-	local blended_rr = front_color_rr * INVERTED_ALPHA_CHANNEL / 255 + back_color_rr * alpha_channel / 255
-	local blended_gg = front_color_gg * INVERTED_ALPHA_CHANNEL / 255 + back_color_gg * alpha_channel / 255
-	local blended_bb = front_color_bb * INVERTED_ALPHA_CHANNEL / 255 + back_color_bb * alpha_channel / 255
+	local blended_rr = foreground_image_color_rr * INVERTED_ALPHA_CHANNEL / 255 + background_screen_color_rr * alpha_channel / 255
+	local blended_gg = foreground_image_color_gg * INVERTED_ALPHA_CHANNEL / 255 + background_screen_color_gg * alpha_channel / 255
+	local blended_bb = foreground_image_color_bb * INVERTED_ALPHA_CHANNEL / 255 + background_screen_color_bb * alpha_channel / 255
 
 	return RGBtoHEX( blended_rr, blended_gg, blended_bb )
 end
 
 --Конвертация 24 битной палитры в 8 битную
---Convert 24bit palette to 8bit
 local function HEX_color24to8( hexcolor24 )
 	local rr, gg, bb = HEXtoRGB( hexcolor24 )
+	local reduced = math.modf(rr / 0x33) * 0x330000 + math.modf(gg / 0x24) * 0x2400 + math.modf(bb / 0x3F) * 0x3F
 
-	return bit32.lshift( bit32.rshift(rr, 5), 5 ) + bit32.lshift( bit32.rshift(gg, 5), 2 ) + bit32.rshift(bb, 6)
+	for i, color in pairs(palette) do
+		if ( hexcolor24 == color ) then
+			return i
+		elseif ( reduced == color ) then
+			return i
+		end
+	end
+
+	return 0
 end
 
 --Конвертация 8 битной палитры в 24 битную
---Convert 8bit palette to 24bit
 local function HEX_color8to24( hexcolor8 )
-	local rr = bit32.lshift( bit32.rshift( hexcolor8, 5 ), 5 )
-	local gg = bit32.lshift( bit32.rshift( bit32.band( hexcolor8, 28 ), 2 ), 5 )
-	local bb = bit32.lshift( bit32.band( hexcolor8, 3 ), 6 )
-
-	return RGBtoHEX( rr, gg, bb )
+	return palette[hexcolor8]
 end
 
+--Сжатие пикселя, исключая символ
 local function compressPixel(foreground, background, alpha)
-	return bit32.lshift( foreground, BYTE*2 ) + bit32.lshift( background, BYTE ) + alpha
+	return bit32.lshift( foreground, BYTE_SIZE*2 ) + bit32.lshift( background, BYTE_SIZE ) + alpha
 end
 
+--Восстановление пикселя
 local function decompressPixel( compressed_pixel )
-	return bit32.rshift( compressed_pixel, BYTE*2 ), bit32.rshift( bit32.band( compressed_pixel, 0x00FF00 ), BYTE ), bit32.band( compressed_pixel, 0x0000FF )
+	return bit32.rshift( compressed_pixel, BYTE_SIZE*2 ), bit32.rshift( bit32.band( compressed_pixel, 0x00FF00 ), BYTE_SIZE ), bit32.band( compressed_pixel, 0x0000FF )
 end
 
 --Подготавливает цвета и символ для записи в файл
---Preparation colors and char from write to file
 local function encodePixel(compressed_pixel, char)
-	--split hex-colors
-	local new_fg, new_bg, alpha = decompressPixel( compressed_pixel )
+	local decompressed_fg, decompressed_bg, alpha = decompressPixel( compressed_pixel )
 	local ascii_char1, ascii_char2, ascii_char3, ascii_char4, ascii_char5, ascii_char6 = string.byte( char, 1, 6 )
 
 	ascii_char1 = ascii_char1 or NULL_CHAR
 
-	return new_fg, new_bg, alpha, ascii_char1, ascii_char2, ascii_char3, ascii_char4, ascii_char5, ascii_char6
+	return decompressed_fg, decompressed_bg, alpha, ascii_char1, ascii_char2, ascii_char3, ascii_char4, ascii_char5, ascii_char6
 end
 
-local function encodePixel_hf(hexcolor_fg, hexcolor_bg, alpha, char)
-	--split hex-colors
-	local new_fg = HEX_color24to8( hexcolor_fg )
-	local new_bg = HEX_color24to8( hexcolor_bg )
+local function encodePixel_full_array(hexcolor_fg, hexcolor_bg, alpha, char)
+	local decompressed_fg = HEX_color24to8( hexcolor_fg )
+	local decompressed_bg = HEX_color24to8( hexcolor_bg )
 	local ascii_char1, ascii_char2, ascii_char3, ascii_char4, ascii_char5, ascii_char6 = string.byte( char, 1, 6 )
 
 	ascii_char1 = ascii_char1 or NULL_CHAR
 
-	return new_fg, new_bg, alpha, ascii_char1, ascii_char2, ascii_char3, ascii_char4, ascii_char5, ascii_char6
+	return decompressed_fg, decompressed_bg, alpha, ascii_char1, ascii_char2, ascii_char3, ascii_char4, ascii_char5, ascii_char6
 end
 
 --Декодирование utf8 символа
---Decode utf8 char
 local function decodeChar(file)
 	local first_byte = readBytes(file, 1)
 	local charcode_array = {first_byte}
@@ -179,9 +189,8 @@ local function decodeChar(file)
 end
 
 --Запись в файл по массиву изображения
---Write image array to file
-function imageAPI.write(path, image, human_form)
-	local elementCount = human_form and ELEMENT_COUNT_HF or ELEMENT_COUNT
+function imageAPI.write(path, image, full_array)
+	local elementCount = full_array and ELEMENT_COUNT_FULL_ARRAY or ELEMENT_COUNT
 	local encodedPixel = nil
 	local file = assert( io.open(path, "w"), FILE_OPEN_ERROR )
 
@@ -190,27 +199,31 @@ function imageAPI.write(path, image, human_form)
 	file:write( string.char( image[IMAGE_HEIGHT] ) )
 	
 	for element = elementCount, image[IMAGE_HEIGHT] * image[IMAGE_WIDTH] * elementCount, elementCount do
-		if ( human_form ) then
+		if ( full_array ) then
+		--Погдотовка к записи по полному массиву(full_array)
 			encodedPixel =
 			{
-				encodePixel_hf
+				encodePixel_full_array
 				(
-					image[IMAGE][element-IMAGE_FG],
-					image[IMAGE][element-IMAGE_BG],
-					image[IMAGE][element-IMAGE_AL],
-					image[IMAGE][element-IMAGE_CH]
+					image[IMAGE][element-FOREGROUND],
+					image[IMAGE][element-BACKGROUND],
+					image[IMAGE][element-ALPHA_CHANNEL],
+					image[IMAGE][element-UTF8_CHAR]
 				)
 			}
 		else
+		--Погдотовка к записи по сжатому массиву
 			encodedPixel =
 			{
 				encodePixel
 				(
-					image[IMAGE][element-IMAGE_COMPRESS],
-					image[IMAGE][element-IMAGE_CH]
+					image[IMAGE][element-COMPRESSED_PIXEL],
+					image[IMAGE][element-UTF8_CHAR]
 				)
 			}
 		end
+
+		--Запись
 		for i = 1, #encodedPixel do
 			file:write( string.char( encodedPixel[i] ) )
 		end
@@ -220,15 +233,15 @@ function imageAPI.write(path, image, human_form)
 end
 
 --Чтение из файла, возвращет массив изображения
---Read from file, return image array
 function imageAPI.read(path)
 	local image = {}
 	local file = assert( io.open(path, "rb"), FILE_OPEN_ERROR )
 
+	--Чтение подписи файла
 	local signature1, signature2 = readBytes(file, 4), readBytes(file, 3)
 	if ( signature1 ~= ocif_signature1 or signature2 ~= ocif_signature2 ) then
 		file:close()
-		return nil
+		error( SIGNATURE_MATCH_ERROR )
 	end
 
 	image[IMAGE_WIDTH]  = readBytes(file, 1)
@@ -237,8 +250,8 @@ function imageAPI.read(path)
 	image[IMAGE] = {}
 
 	for element = ELEMENT_COUNT, image[IMAGE_HEIGHT] * image[IMAGE_WIDTH] * ELEMENT_COUNT, ELEMENT_COUNT do
-		image[IMAGE][element-IMAGE_COMPRESS] = readBytes(file, 3)
-		image[IMAGE][element-IMAGE_CH]       = decodeChar( file )
+		image[IMAGE][element-COMPRESSED_PIXEL] = readBytes(file, 3)
+		image[IMAGE][element-UTF8_CHAR]       = decodeChar( file )
 	end
 
 	file:close()
@@ -246,8 +259,7 @@ function imageAPI.read(path)
 	return image
 end
 
---Отрисовка изображения
---Draw image
+--Отрисовка изображения(для примера. Медленная. Делайте СВОЮ отрисовку)
 function imageAPI.draw(image, sx, sy, gpu)
 	local x, y = 0, 0
 	local prevBG, prevFG = nil, nil
@@ -257,11 +269,11 @@ function imageAPI.draw(image, sx, sy, gpu)
 		y = (x == 1) and y+1 or y
 
 		local _, _, back_color = gpu.get(sx+x-1, sy+y-1)
-		local fg8, bg8, al = decompressPixel( image[IMAGE][element-IMAGE_COMPRESS] )
+		local fg8, bg8, al = decompressPixel( image[IMAGE][element-COMPRESSED_PIXEL] )
 		local fg24, bg24 = HEX_color8to24( fg8 ), HEX_color8to24( bg8 )
 
 		currentFG = fg24
-		currentBG = alpha_blend( back_color, bg24, al )
+		currentBG = alphaBlend( back_color, bg24, al )
 
 		if ( currentFG ~= prevFG ) then
 			prevFG = currentFG
@@ -272,7 +284,7 @@ function imageAPI.draw(image, sx, sy, gpu)
 			gpu.setBackground(currentBG)
 		end
 
-		gpu.set(sx+x-1, sy+y-1, image[IMAGE][element-IMAGE_CH])
+		gpu.set(sx+x-1, sy+y-1, image[IMAGE][element-UTF8_CHAR])
 	end
 end
 
